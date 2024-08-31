a9e1559c9e44e1699a30355cce3ab16259b20200
Kurzgesagt <lucasmorenorodrigues@hotmail.com>
2024-08-30 22:10:35 -0300

    alterações no menu

--- funcoes.py
+++ funcoes.py
@@ -1,27 +1,5 @@
 from deck_lib import *
 
-def cria_janela():
-    global janela
-    global texto_processado
-    
-    janela = Tk()
-    janela.title("Deck lister")
-
-    texto_orientação = Label(janela,text="Clique no botao para ver a cotação das moedas")
-    texto_orientação.grid(column=0 , row=0)
-
-    botao = Button(janela,text="Selecione o arquivo",command=seleciona_arquivo)
-    botao.grid(column=0,row=1)
-
-    texto_processado = scrolledtext.ScrolledText(janela, wrap=tk.WORD, width=20, height=20)
-    texto_processado.grid(column=0, row=2)
-    texto_processado.configure(state='disabled') 
-
-    botao_copiar = Button(janela,text="Copiar",command=copiar)
-    botao_copiar.grid(column=0,row=3)
-
-    janela.mainloop()
-    
 def seleciona_arquivo():
     caminho_arquivo = filedialog.askopenfilename(
         filetypes=[("CSV files", "*.csv")],
@@ -48,3 +26,26 @@ def copiar():
     janela.clipboard_clear()
     janela.clipboard_append(texto_processado)
     janela.update()
+
+def cria_janela():
+    global janela
+    global texto_processado
+    
+    janela = Tk()
+    janela.title("Deck lister")
+
+    texto_orientação = Label(janela,text="Clique no botao para ver a cotação das moedas")
+    texto_orientação.grid(column=0 , row=0)
+
+    botao = Button(janela,text="Selecione o arquivo",command=seleciona_arquivo)
+    botao.grid(column=0,row=1)
+
+    texto_processado = scrolledtext.ScrolledText(janela, wrap=tk.WORD, width=20, height=20)
+    texto_processado.grid(column=0, row=2)
+    texto_processado.configure(state='disabled') 
+
+    botao_copiar = Button(janela,text="Copiar",command=copiar)
+    botao_copiar.grid(column=0,row=3)
+
+    janela.mainloop()
+    
